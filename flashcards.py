0c6cad6c9d0079412269939713a2ceccafcda5da
Nikesh Biraggari <144965119+Nikesh984@users.noreply.github.com>
2024-05-14 18:42:42 -0400

    Add files via upload

---
+++ flashcards.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from datetime import datetime
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to flashcards application!"
+
+
+@app.route("/date")
+def date():
+    return "This page was served at " + str(datetime.now())
+
+
+counter = 0
+
+@app.route("/count_views")
+def count_views():
+    global counter
+    counter += 1
+    return "This page was served " + str(counter) + " times"
